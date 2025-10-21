0f887772126458be6a26a391f66ce506444a7043
Alejandro <alejodev2022@gmail.com>
2025-10-21 12:26:50 -0500

    style: simplify PublicLayout component and update background color

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,18 +1,13 @@
-
-// 1. Importa Outlet
 import { Outlet } from 'react-router-dom';
 
 const PublicLayout = () => {
-  return (
-    <div className="flex min-h-screen flex-col">
-     
-      <main className="flex-grow">
-      
-        <Outlet />
-      </main>
-      
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
 };
 
 export default PublicLayout;
