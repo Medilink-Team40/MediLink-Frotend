913eb780b62ca1d0c23da103123ecff34ced89d3
Alejandro <alejodev2022@gmail.com>
2025-10-26 02:10:14 -0500

    fix: corregir la ruta de importación del archivo de estilos en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import ErrorBoundary from '@/components/common/ErrorBoundary';
-import '@/style/index.css';
+import './index.css';
 
 const container = document.getElementById('root');
 
