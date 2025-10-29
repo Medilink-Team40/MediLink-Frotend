0a4586a83aed2f1ff2f3d4b74dc012fcaac2a693
Alejandro <alejodev2022@gmail.com>
2025-10-28 23:19:51 -0500

    refactor(roles): corregir valores de roles para consistencia en el sistema

--- src/routes/roles.ts
+++ src/routes/roles.ts
@@ -1,8 +1,8 @@
 export const ROLES = {
-    ADMIN: 'admin',
-    PACIENTE: 'paciente',
-    DOCTOR: 'doctor'
-} as const ;
+  ADMIN: 'admin',
+  DOCTOR: 'practitioner', 
+  PACIENTE: 'patient',
+} as const;
 
 
 export type Roles = typeof ROLES[keyof typeof ROLES];
