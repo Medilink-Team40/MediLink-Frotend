c2e8d8a417f8e44cfcacd2341b96f8478e58214c
Alejandro <alejodev2022@gmail.com>
2025-10-28 21:09:17 -0500

    fix(keycloak): corregir rol de usuario 'doctor' a 'practitoner' en la función getDashboardPathByRole

--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -27,7 +27,7 @@ export const getDashboardPathByRole = (roles: string[]): string => {
     if (roles.includes('admin')) {
         return '/admin/dashboard';
     }
-    if (roles.includes('doctor')) {
+    if (roles.includes('practitoner')) {
         return '/doctor/dashboard';
     }
     if (roles.includes('patient')) {
