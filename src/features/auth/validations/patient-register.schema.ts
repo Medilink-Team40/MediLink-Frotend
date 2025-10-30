eaacf993bbcec964f0beb0f076e31deb0639bfee
Alejandro <alejodev2022@gmail.com>
2025-10-30 07:40:44 -0500

    fix(PatientRegisterSchema): agregar campo DNI como opcional en el esquema de registro

--- src/features/auth/validations/patient-register.schema.ts
+++ src/features/auth/validations/patient-register.schema.ts
@@ -8,7 +8,7 @@ export const patientRegisterSchema = z.object({
   phone: z.string().min(8, "Número de teléfono inválido"),
   birthDate: z.string().min(1, "La fecha de nacimiento es requerida"),
   gender: z.enum(['male', 'female', 'other', 'unknown']),
- 
+ dni: z.string().optional(),
   password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres"),
   confirmPassword: z.string()
 }).refine((data) => data.password === data.confirmPassword, {
