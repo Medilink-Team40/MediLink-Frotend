341941c76f66e6f8617da3ba75d9b02e6781a3f7
Alejandro <alejodev2022@gmail.com>
2025-10-30 07:44:14 -0500

    feat(patient.types): agregar interfaces HumanName y ContactPoint para la gestión de datos del paciente

--- src/types/patient.types.ts
+++ src/types/patient.types.ts
@@ -7,14 +7,52 @@ export enum FHIRExternalGender {
   UNKNOWN = 'unknown'
 }
 
-interface PatientData {
-  
- name: string,
+interface HumanName {
+  use: 'official' | 'nickname' | 'anonymous' | 'old' | 'maiden';
+  text: string;
+  family: string;
+  given: string[];
+  prefix?: string[];
+  suffix?: string[];
+}
+
+interface ContactPoint {
+  id?: string;
+  system: 'phone' | 'fax' | 'email' | 'pager' | 'url' | 'sms' | 'other';
+  value: string;
+  use?: 'home' | 'work' | 'temp' | 'old' | 'mobile';
+  rank?: number;
+  period?: {
+    start?: string;
+    end?: string;
+  };
+}
+
+export interface PatientRegisterData {
  email: string,
  password: string,
  repeatpassword: string,
  birthDate: string,
  gender: string | FHIRExternalGender,
+ name: HumanName[],
+ telecom?: ContactPoint[],
+
 }
 
-export default PatientData;
+interface PatientFormData {
+  firstName: string;       
+  lastName: string;
+  email: string;
+  password?: string;
+  repeatpassword?: string;
+  birthDate: string;
+  gender: string | FHIRExternalGender;
+  name: HumanName[];
+  phone: string;
+  dni?: string;
+
+}
+
+export type { HumanName, ContactPoint, PatientFormData}
+
+export default PatientRegisterData;
