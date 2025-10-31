b4c443687f6165047310478011ebb3f303d47f37
Alejandro Anchundia <118407195+anchundiatech@users.noreply.github.com>
2025-10-31 17:44:20 -0500

    Change background gradient to blue in HeroSection

--- src/features/landing/components/HeroSection.tsx
+++ src/features/landing/components/HeroSection.tsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
   }
 
   return (
-    <section className="relative bg-gradient-to-br from-gray-50 to-white overflow-hidden min-h-[600px] sm:min-h-[700px] lg:min-h-[800px]">
+    <section className="relative bg-linear-to-br from-gray-50 to-blue-400  overflow-hidden min-h-[600px] sm:min-h-[700px] lg:min-h-[800px]">
       <div className="max-w-7xl mx-auto relative">
         <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-32">
